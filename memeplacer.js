def908e0bd26b05cddc756e64e9f2e047b0ae289
Tyler Longren <tlongren@gmail.com>
2014-10-23 06:56:41 -0500

    Debug nodetime.

--- memeplacer.js
+++ memeplacer.js
@@ -1,7 +1,8 @@
 if(process.env.NODETIME_ACCOUNT_KEY) {
   require('nodetime').profile({
     accountKey: process.env.NODETIME_ACCOUNT_KEY,
-    appName: 'Placezombie' // optional
+    appName: 'Placezombie', // optional
+    debug:true
   });
 }
 
