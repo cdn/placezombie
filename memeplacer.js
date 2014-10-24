eb420587355f8c201a44f0e55b2f3fdbf9a81c5f
Tyler Longren <tlongren@gmail.com>
2014-10-24 08:53:43 -0500

    Add gaug.es to node.

--- memeplacer.js
+++ memeplacer.js
@@ -49,6 +49,18 @@ function listener(request, response) {
   } else {
     notFound(response);
   }
+
+  var _gauges = _gauges || [];
+  (function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '54488572eddd5b04ac0019cf');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
 }
 
 /**
